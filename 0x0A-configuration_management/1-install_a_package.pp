b20cf24d0f901788c0c354f6d4a40acc9fa589e1
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-02-28 17:01:24 +0100

    install pip before install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Using Puppet, install flask from pip3
+
+# install pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
-  ensure   => installed,
-  version  => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
