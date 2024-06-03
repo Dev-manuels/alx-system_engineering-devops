3898e3050c28a9299e64b33408d693aa7378c87e
Dev Manuels <Emmanuel.umeile@gmail.com>
2024-06-03 01:29:41 +0100

    Deferal process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# a manifest that kills a process named killmenow
-exec { 'pkill_process':
-  command => '/bin/pkill -SIGTERM killmenow',
-  onlyif  => '/bin/ps -ef | /bin/grep killmenow | /bin/grep -v grep',
-}
